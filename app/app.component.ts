400732ef5bd2bb81c5a992982b2e6353f769183c
Abebe <abetarch@gmail.com>
2017-04-21 10:58:22 -0500

    final

--- app/app.component.ts
+++ app/app.component.ts
@@ -6,6 +6,7 @@ import { DataService } from './service/data.service';
 @Component({
   selector: 'app-root',
   template: `
+  <input #item type=''text' >
     <div *ngFor="let post of posts | mypipe:pipeargs">
 <mypost [data]="post"></mypost>
 </div>
--- app/service/mypost.component.ts
+++ app/service/mypost.component.ts
@@ -4,21 +4,26 @@ import { DataService } from './data.service';
 @Component({
   selector: 'mypost',
   template: `
-
+{{posts.title}}
   `
 })
 export class MypostComponent {
     value = '';
-    items: Object;
+    posts: string[];
 
     constructor (private dataService: DataService) {}
     
     getPost() {
     this.dataService.getPost().subscribe(
-        response => this.items=response,
+        response => this.posts=response,
         error => console.error(error),
         ()=> console.log('finished')
     );
+    }
+    condition(item){
+        if(this.posts.contains(item))
+        return true;
+        else return false;
     }
 
     
